3721cc0d9608d66e22281074a306c44d78e71425
unknown <alexishadjian@gmail.com>
2023-05-13 16:53:46 +0200

    Homepage pres section

--- Views/homepage.php
+++ Views/homepage.php
@@ -4,20 +4,34 @@
             <h1 class="content__title"><?= $datas["title"] ?></h1>
             <p class="content__description"><?= $datas["content"] ?></p>
         </div>
-        <a id="scroll-btn" href="<?= SITE_URL ?>/#tmp">
+        <a id="scroll-btn" href="<?= SITE_URL ?>/#presentation">
             <span></span>
             <span></span>
             <span></span>
         </a>
     </section>
-    <section id="tmp">
-
-        <p>tmpV2</p>
+    <section id="presentation">
+        <div class="wrapper">
+            <div class="left">
+                <div class="big-img" style="background-image: url('public/assets/img/voilier.jpg');">
+                    <div class="small-img" style="background-image: url('public/assets/img/yatch.jpg');"></div>
+                </div>
+            </div>
+            <div class="right">
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore consequatur officia molestias harum nisi aspernatur non dolore adipisci doloribus quisquam quidem vitae animi possimus sequi quod pariatur at, voluptatem sapiente.</p>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore consequatur officia molestias harum nisi aspernatur non dolore adipisci doloribus quisquam quidem vitae animi possimus sequi quod pariatur at, voluptatem sapiente.</p>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore consequatur officia molestias harum nisi aspernatur non dolore adipisci doloribus quisquam quidem vitae animi possimus sequi quod pariatur at, voluptatem sapiente.</p>
+            </div>
+        </div>
+        
 
     </section>
     <section id="tmp2">
 
-
+    <p>tmpV2</p>
 
     </section>
 </div>
